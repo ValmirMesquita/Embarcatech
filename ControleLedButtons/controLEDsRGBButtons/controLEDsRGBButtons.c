e8341243566d433294991ebd35a46ebe8e99f922
Valmir L Sousa Mesquita <valmirsmesquita@gmail.com>
2025-04-19 22:36:49 -0300

    Embarcatech2

--- ControleLedButtons/controLEDsRGBButtons/controLEDsRGBButtons.c
+++ ControleLedButtons/controLEDsRGBButtons/controLEDsRGBButtons.c
@@ -7,36 +7,17 @@
 #define BTN_A_PIN 5
 #define BTN_B_PIN 6
 
-/*seta os pinos definidos (#define) conforme a variavel da função set_leds*/
-void set_leds(bool red, bool green, bool blue )
-{
-    gpio_put(LED_R_PIN, red);   /*Coloca o pino LED_R_PIN como red*/
-    gpio_put(LED_G_PIN, green); /*Coloca o pino LED_G_PIN como green*/
-    gpio_put(LED_B_PIN, blue);  /*Coloca o pino LED_B_PIN como blue*/
-}
+/*--------- Prototipo das funçoes ---------- */
+void set_leds(bool red, bool green, bool blue );
+void set_pin();
+
+
 
 int main()
 {
     stdio_init_all();
 
-    // Definiçoes de saida dos leds como saida
-    gpio_init(LED_R_PIN);
-    gpio_set_dir(LED_R_PIN, GPIO_OUT);
-    
-    gpio_init(LED_G_PIN);
-    gpio_set_dir(LED_G_PIN, GPIO_OUT);
-    
-    gpio_init(LED_B_PIN);
-    gpio_set_dir(LED_B_PIN, GPIO_OUT);
-    
-    // Definiçoes de saida dos botoes como entrada 
-    gpio_init(BTN_A_PIN);
-    gpio_set_dir(BTN_A_PIN, GPIO_IN);
-    gpio_pull_up(BTN_A_PIN);
-
-    gpio_init(BTN_B_PIN);
-    gpio_set_dir(BTN_B_PIN, GPIO_IN);
-    gpio_pull_up(BTN_B_PIN);
+    set_pin();
 
     while (true) {
 
@@ -72,4 +53,38 @@ int main()
     }
 
     return 0;
+}
+
+/*Declaração das funcoes */
+
+/*Defiçoões da pinagem de saidas dos pinos leds e botoes */
+void set_pin()
+{
+    // Definiçoes de saida dos leds como saida
+    gpio_init(LED_R_PIN);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+    
+    gpio_init(LED_G_PIN);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    
+    gpio_init(LED_B_PIN);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+    
+    // Definiçoes de saida dos botoes como entrada 
+    gpio_init(BTN_A_PIN);
+    gpio_set_dir(BTN_A_PIN, GPIO_IN);
+    gpio_pull_up(BTN_A_PIN);
+
+    gpio_init(BTN_B_PIN);
+    gpio_set_dir(BTN_B_PIN, GPIO_IN);
+    gpio_pull_up(BTN_B_PIN);
+}
+
+
+/*seta os pinos definidos (#define) conforme a variavel da função set_leds*/
+void set_leds(bool red, bool green, bool blue )
+{
+    gpio_put(LED_R_PIN, red);   /*Coloca o pino LED_R_PIN como red*/
+    gpio_put(LED_G_PIN, green); /*Coloca o pino LED_G_PIN como green*/
+    gpio_put(LED_B_PIN, blue);  /*Coloca o pino LED_B_PIN como blue*/
 }
